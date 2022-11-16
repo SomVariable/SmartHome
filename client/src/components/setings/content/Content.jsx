170191a97f396143d0ffe5404a26326eafe0c8c3
SomVariable <79557545+SomVariable@users.noreply.github.com>
2022-11-16 13:50:09 +0200

    working with settings

--- client/src/components/setings/content/Content.jsx
+++ client/src/components/setings/content/Content.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
-import { Route, Switch } from 'react-router-dom'
 import style from "./style.module.scss"
 
 
 function Content({currentComponent, chooseContent}) {
+  
   return (
     <div className = {style.settingsContent}>
       {chooseContent(currentComponent)}
--- client/src/components/setings/settingsComponents/homeSettings/HomeSettings.jsx
+++ client/src/components/setings/settingsComponents/homeSettings/HomeSettings.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useState } from 'react';
+import { Popover } from '../../../modal/Popover';
+import style from "./style.module.scss"
+
 
 function HomeSettings() {
+  const [modules, setModulesOpened] = useState(false);
+  const homeSettingsRef = useRef()
+  debugger
   return (
-    <div>HomeSettings</div>
+    <div className = {style.homeSettings} ref = {homeSettingsRef}>
+      <Popover opened={modules} onClose={() => setModulesOpened(false)} parent = {homeSettingsRef.current}> 
+        <div className="">
+          <p>Humidity</p>
+          <p>Temperature</p>
+          <p>Light</p>
+          <p>Humidity</p>
+          <p>Temperature</p>
+          <p>Light</p>
+          <p>Humidity</p>
+          <p>Light</p>
+          <p>Humidity</p>
+          <p>Light</p>
+        </div>
+      
+      </Popover>
+      <button className = {style.moduleAdditionBtn} onClick = {() => setModulesOpened(true)}>add module</button>
+    </div>
   )
 }
 
